e0f4783701fed0f8401aa6a709bf699a917c0496
Ashfaque Khan <42895491+ashfaquekhan@users.noreply.github.com>
2024-08-02 23:24:27 +0530

    UVC_TEST

--- main/main.c
+++ main/main.c
@@ -1,25 +1,19 @@
 #include <esp_log.h>
 #include <esp_system.h>
 #include <nvs_flash.h>
-#include <sys/param.h>
 #include <string.h>
-
+#include <sys/param.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-
-// support IDF 5.x
-#ifndef portTICK_RATE_MS
-#define portTICK_RATE_MS portTICK_PERIOD_MS
-#endif
-
 #include "esp_camera.h"
+#include "usb_device_uvc.h"
+#include "uvc_frame_config.h"
 
 #define CAM_PIN_PWDN -1  //power down is not used
 #define CAM_PIN_RESET -1 //software reset will be performed
 #define CAM_PIN_XCLK 4
 #define CAM_PIN_SIOD 33
 #define CAM_PIN_SIOC 34
-
 #define CAM_PIN_D7 18
 #define CAM_PIN_D6 17
 #define CAM_PIN_D5 16
@@ -35,14 +29,12 @@
 
 static const char *TAG = "Test";
 
-#if ESP_CAMERA_SUPPORTED
 static camera_config_t camera_config = {
     .pin_pwdn = CAM_PIN_PWDN,
     .pin_reset = CAM_PIN_RESET,
     .pin_xclk = CAM_PIN_XCLK,
     .pin_sccb_sda = CAM_PIN_SIOD,
     .pin_sccb_scl = CAM_PIN_SIOC,
-
     .pin_d7 = CAM_PIN_D7,
     .pin_d6 = CAM_PIN_D6,
     .pin_d5 = CAM_PIN_D5,
@@ -54,56 +46,78 @@ static camera_config_t camera_config = {
     .pin_vsync = CAM_PIN_VSYNC,
     .pin_href = CAM_PIN_HREF,
     .pin_pclk = CAM_PIN_PCLK,
-
-    //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
     .xclk_freq_hz = 20000000,
     .ledc_timer = LEDC_TIMER_0,
     .ledc_channel = LEDC_CHANNEL_0,
-
-    .pixel_format = PIXFORMAT_RGB565, //YUV422,GRAYSCALE,RGB565,JPEG
-    .frame_size = FRAMESIZE_QVGA,    //QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
-
-    .jpeg_quality = 30, //0-63, for OV series camera sensors, lower number means higher quality
-    .fb_count = 1,       //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
+    .pixel_format = PIXFORMAT_JPEG,
+    .frame_size = FRAMESIZE_QVGA,
+    .jpeg_quality = 30,
+    .fb_count = 1,
     .fb_location = CAMERA_FB_IN_PSRAM,
     .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
 };
 
-static esp_err_t init_camera(void)
-{
-    //initialize the camera
+static esp_err_t init_camera(void) {
     esp_err_t err = esp_camera_init(&camera_config);
-    if (err != ESP_OK)
-    {
+    if (err != ESP_OK) {
         ESP_LOGE(TAG, "Camera Init Failed");
         return err;
     }
-
     return ESP_OK;
 }
-#endif
 
-void app_main(void)
-{
-#if ESP_CAMERA_SUPPORTED
-    if(ESP_OK != init_camera()) {
+static esp_err_t camera_start_cb(uvc_format_t format, int width, int height, int rate, void *cb_ctx) {
+    ESP_LOGI(TAG, "Camera Start");
+    return ESP_OK;
+}
+
+static void camera_stop_cb(void *cb_ctx) {
+    ESP_LOGI(TAG, "Camera Stop");
+}
+
+static uvc_fb_t* camera_fb_get_cb(void *cb_ctx) {
+    camera_fb_t *cam_fb = esp_camera_fb_get();
+    if (!cam_fb) {
+        return NULL;
+    }
+    static uvc_fb_t uvc_fb;
+    uvc_fb.buf = cam_fb->buf;
+    uvc_fb.len = cam_fb->len;
+    uvc_fb.width = cam_fb->width;
+    uvc_fb.height = cam_fb->height;
+    uvc_fb.format = cam_fb->format;
+    uvc_fb.timestamp = cam_fb->timestamp;
+    return &uvc_fb;
+}
+
+static void camera_fb_return_cb(uvc_fb_t *fb, void *cb_ctx) {
+    esp_camera_fb_return((camera_fb_t *)fb->buf);
+}
+
+void app_main(void) {
+    if (ESP_OK != init_camera()) {
         return;
     }
 
-    while (1)
-    {
-        ESP_LOGI(TAG, "Taking picture...");
-        camera_fb_t *pic = esp_camera_fb_get();
-
-        // use pic->buf to access the image
-        ESP_LOGI(TAG, "Picture taken! Its size was: %zu bytes", pic->len);
-
-        esp_camera_fb_return(pic);
-
-        vTaskDelay(5000 / portTICK_RATE_MS);
+    uint8_t *uvc_buffer = (uint8_t *)malloc(64 * 1024);
+    if (uvc_buffer == NULL) {
+        ESP_LOGE(TAG, "malloc frame buffer fail");
+        return;
     }
-#else
-    ESP_LOGE(TAG, "Camera support is not available for this chip");
-    return;
-#endif
+
+    uvc_device_config_t config = {
+        .uvc_buffer = uvc_buffer,
+        .uvc_buffer_size = 64 * 1024,
+        .start_cb = camera_start_cb,
+        .fb_get_cb = camera_fb_get_cb,
+        .fb_return_cb = camera_fb_return_cb,
+        .stop_cb = camera_stop_cb,
+    };
+
+    ESP_ERROR_CHECK(uvc_device_config(0, &config));
+    ESP_ERROR_CHECK(uvc_device_init());
+
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
 }
